1cf863a6deea466ab936b5fa1aa5bf1d1c3a03c0
Marton Tasnadi <marton.tasnadi@gmail.com>
2021-01-03 22:55:39 +0100

    Initial version

---
+++ app.php
@@ -0,0 +1,26 @@
+<?php
+
+require './vendor/autoload.php';
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+const BASE_URL = 'https://telex.hu/';
+$client = new Client();
+
+$latest_page = $client->request('GET', BASE_URL . 'legfrissebb');
+$last_page = $latest_page->filter('.pagination .page:last-child')->attr('href');
+$last_page = (int) explode('=', $last_page)[1];
+
+$article_urls = [];
+for ($i = 1; $i <= $last_page; $i++) {
+    $page = $client->request('GET', BASE_URL . 'legfrissebb?oldal=' . $i);
+    $page->filter('.article_title')->each(function (Crawler $node) use ($client, &$article_urls) {
+        $article_url = BASE_URL . $node->attr('href');
+        array_push($article_urls, $article_url);
+    });
+}
+
+$article_urls = array_values(array_unique($article_urls));
+foreach ($article_urls as $url) {
+
+}
